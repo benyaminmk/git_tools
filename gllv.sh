b99a6cb81bfd2d7cb9277cc40b2588e8c94c898a
Benyamin Meschede-Krasa <benmk@stanford.edu>
2024-11-07 11:56:02 -0800

    case sentitivity doenst work on mac so had to rename files

---
+++ gllv.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+#/ Usage: Gl.sh COMMIT_RANGE
+#/ Description: watch a live git log with color
+#/ Examples: Gl.sh master..develop
+#/ Options:
+#/   --help: Display this help message
+usage() { grep '^#/' "$0" | cut -c4- ; exit 0 ; }
+expr "$*" : ".*--help" > /dev/null && usage
+
+
+COMMIT_RANGE=${1:---all}
+FORMAT_GIT_LOG="%C(auto)%h %Cblue%cr%Creset%C(auto) %d %s"
+
+watch --color -n1 --no-title \
+    "git log --color --graph --oneline --pretty=\"${FORMAT_GIT_LOG}\" ${COMMIT_RANGE}"
+#                                               ^ enquote expansion of formatting variable
+
